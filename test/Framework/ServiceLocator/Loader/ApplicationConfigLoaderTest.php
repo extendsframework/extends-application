8765fff6cc2fce61034bf878d030b264341ed871
Vincent van Dijk <info@vincentvandijk.com>
2017-12-07 17:06:43 +0100

    Added middlewares to config loader.

--- test/Framework/ServiceLocator/Loader/ApplicationConfigLoaderTest.php
+++ test/Framework/ServiceLocator/Loader/ApplicationConfigLoaderTest.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 namespace ExtendsFramework\Application\Framework\ServiceLocator\Loader;
 
 use ExtendsFramework\Application\ApplicationInterface;
+use ExtendsFramework\Application\Framework\Http\Middleware\NotImplementedMiddleware;
 use ExtendsFramework\Application\Framework\ServiceLocator\Factory\ApplicationFactory;
+use ExtendsFramework\Authentication\Framework\Http\Middleware\NotAuthenticatedMiddleware;
+use ExtendsFramework\Authorization\Framework\Http\Middleware\NotAuthorizedMiddleware;
+use ExtendsFramework\Http\Framework\Http\Middleware\Exception\ExceptionMiddleware;
+use ExtendsFramework\Http\Framework\Http\Middleware\Renderer\RendererMiddleware;
+use ExtendsFramework\Http\Middleware\Chain\MiddlewareChainInterface;
+use ExtendsFramework\Logger\Framework\Http\Middleware\Logger\LoggerMiddleware;
+use ExtendsFramework\Router\Framework\Http\Middleware\Controller\ControllerMiddleware;
+use ExtendsFramework\Router\Framework\Http\Middleware\Router\RouterMiddleware;
+use ExtendsFramework\Security\Framework\Http\Middleware\RouterAuthorizationMiddleware;
 use ExtendsFramework\ServiceLocator\Resolver\Factory\FactoryResolver;
 use ExtendsFramework\ServiceLocator\ServiceLocatorInterface;
 use PHPUnit\Framework\TestCase;
@@ -25,8 +35,19 @@ class ApplicationConfigLoaderTest extends TestCase
         $this->assertSame([
             ServiceLocatorInterface::class => [
                 FactoryResolver::class => [
-                    ApplicationInterface::class => ApplicationFactory::class
+                    ApplicationInterface::class => ApplicationFactory::class,
                 ],
+            ],
+            MiddlewareChainInterface::class => [
+                RendererMiddleware::class => 200,
+                ExceptionMiddleware::class => 190,
+                LoggerMiddleware::class => 175,
+                RouterMiddleware::class => 150,
+                NotAuthorizedMiddleware::class => 150,
+                NotAuthenticatedMiddleware::class => 130,
+                RouterAuthorizationMiddleware::class => 120,
+                ControllerMiddleware::class => 100,
+                NotImplementedMiddleware::class => 90,
             ],
         ], $loader->load());
     }
